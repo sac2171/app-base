19dd18dac29775fc8d6044203f2b98905334d3b0
Shane Chin <shanechin@Shanes-MacBook-Pro.local>
2017-11-03 02:06:49 -0700

    Getting data back from the server

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,36 @@
 import * as React from 'react';
 import './App.css';
+import 'whatwg-fetch'
 
 const logo = require('./logo.svg');
 
 class App extends React.Component {
+
+  componentDidMount() {
+    let propKey = 'initial_props';
+    let csrf = 'csrf';
+
+    global.console.log(
+        window[propKey],
+        'Jank Django => React communication'
+    );
+
+    let headers = new Headers();
+    headers.append("X-CSRFToken", window[csrf])
+    fetch('/' , {
+        method: "POST",
+        credentials:'include',
+        headers:headers,
+        body : JSON.stringify({
+          month:true,
+        })
+      }).then((resp) => resp.json())
+      .then(data => {
+        global.console.log(data)
+      }
+    )
+  }
+
   render() {
     return (
       <div className="App">
